413af7373d089d154961d252a19c6dcb253b94d2
Basir Khan <khanbasir5555@gmail.com>
2023-03-10 15:06:09 +0530

    second commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,12 +4,20 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 const Computers = () => {
-  const Computers= useGLTF('./desktop_pc/scene.gltf');
+  const Computer= useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundcolor='black'/>
+      <hemisphereLight intensity={0.15} groundColor='black'/>
       <pointLight intensity={1}/>
-      <primitive object={Computers.scene}
+      <spotLight
+      position={[-20,50,10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}
+      />
+      <primitive object={Computer.scene}
       scale={0.75}
       position={[0,-3.25,-1.5]}
       rotation={[-0.01,-0.2,-0.1]}
@@ -20,11 +28,10 @@ const Computers = () => {
 const ComputersCanvas =()=>{
   return(
     <Canvas
-     frameLoop="demand" 
+     frameloop="demand" 
      shadows
      camera ={{position: [20,3,5] ,fov: 25}}
-     gl={{preserveDrawingBuffer:true}}
-      >
+     gl={{ preserveDrawingBuffer:true}}>
         <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls enableZoom={false}
         maxPolarAngle={Math.PI/2}
@@ -37,4 +44,4 @@ const ComputersCanvas =()=>{
   )
 }
 
-export default Computers
+export default ComputersCanvas
